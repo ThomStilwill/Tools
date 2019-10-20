ba04a874a9168fd4b348c66730b2687236855fbe
ThomStilwill <thom.stilwill@gmail.com>
2019-10-20 18:28:13 -0400

    traverse folders

--- NodeCleaner/Program.cs
+++ NodeCleaner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace NodeCleaner
 {
@@ -6,7 +7,32 @@ namespace NodeCleaner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Traversing...");
+
+            Traverse(@"D:\Cloud\GoogleDrive\Projects\NG");
+
+            Console.WriteLine("Complete, press any key.");
+            Console.ReadKey();
+        }
+
+
+
+
+        public static void Traverse(string path)
+        {
+            string lastFolderName = Path.GetFileName(Path.GetDirectoryName(path+@"/"));
+
+            if (lastFolderName == "node_modules")
+            {
+                Console.WriteLine(path);
+                return;
+            }
+
+            var directories = Directory.GetDirectories(path);
+            foreach (var directory in directories)
+            {
+               Traverse(directory);
+            }
         }
     }
 }
