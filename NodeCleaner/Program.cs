f33f9de833d3fb0a687b254f2b8c7fe1b84a01fe
ThomStilwill <thom.stilwill@gmail.com>
2019-10-20 18:04:15 -0400

    Add project files.

---
+++ NodeCleaner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NodeCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
